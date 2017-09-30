6bbeb01aa88d220ad95950ffb93ca6987fd67e3c
tsuyukusa-rk <ryoji@kondouryouji-no-MacBook-Pro.local>
2017-10-01 00:18:13 +0900

    ディレクトリのrefactoring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import Top from './components/pages/top/'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Top />, document.getElementById('root'))
