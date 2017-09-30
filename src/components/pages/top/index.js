021d267872189c597a1f232c2f66d96c5db1576f
tsuyukusa-rk <ryoji@kondouryouji-no-MacBook-Pro.local>
2017-10-01 01:50:00 +0900

    update

--- src/components/pages/top/index.js
+++ src/components/pages/top/index.js
@@ -1,43 +1,28 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import logo from '../../../images/logo.svg'
+import MainImage from '../../../images/main.jpg'
 
 export default class App extends Component {
   render () {
     return (
       <Wrapper>
         <Header>
-          <Logo src={logo} alt='logo' />
-          <Title>Welcome to React</Title>
+          <Title>fogGoat</Title>
         </Header>
-        <Intro>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </Intro>
+        <Main>
+          <img src={MainImage} alt='main画像' />
+        </Main>
       </Wrapper>
     )
   }
 }
 
-const Wrapper = styled.div`
-  text-align: center;
-`
+const Wrapper = styled.div``
 const Header = styled.header`
-  background-color: #222;
   height: 150px;
   padding: 20px;
-  color: white;
 `
-const Logo = styled.img`
-  @keyframes App-logo-spin {
-    from { transform: rotate(0deg); }
-    to { transform: rotate(360deg); }
-  }
-  animation: App-logo-spin infinite 20s linear;
-  height: 80px;
-`
-const Title = styled.h1`
-  font-size: 1.5em;
-`
-const Intro = styled.p`
-  font-size: large;
+const Title = styled.h1``
+const Main = styled.main`
+  width: 100%;
 `
