3c34931886562ec27ccf5f0fb064be21ff0fc450
tsuyukusa-rk <ryoji@kondouryouji-no-MacBook-Pro.local>
2017-10-01 08:33:16 +0900

    update

--- src/components/pages/top/index.js
+++ src/components/pages/top/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+
 import { Drawer, MenuItem } from 'material-ui'
+import { Card,  CardText, CardTitle } from 'material-ui/Card'
+
 import mainImage from '../../../images/main.jpg'
 import ThLargeOutline from 'react-icons/lib/ti/th-large-outline'
+import IconCalendar from 'react-icons/lib/go/calendar'
+import IconContact from 'react-icons/lib/md/contact-mail'
+
 import { colors } from '../../../const/'
 const { white } = colors
 
@@ -21,16 +27,45 @@ export default class App extends Component {
           <Title>fogGoat</Title>
           <BtnMenu onClick={this.handleToggle}><ThLargeOutline /></BtnMenu>
           <Sidebar
-            width={200}
+            width={300}
             openSecondary
             open={this.state.open}
             docked={false}
             onRequestChange={(open) => this.setState({open})}
           >
-            <MenuItem onClick={this.handleClose}>Schedule</MenuItem>
+            <MenuItem primaryText="Schedule" leftIcon={<IconCalendar />} onClick={this.handleClose} />
+            <MenuItem primaryText="Contact" leftIcon={<IconContact />} onClick={this.handleClose} />
           </Sidebar>
         </Header>
-        <Main />
+        <Main>
+          <SubTitle>Topics</SubTitle>
+          <CardList>
+            <CardItem>
+              <CardTitle>TitleTitleTitleTitle</CardTitle>
+              <CardText>ああああああああ</CardText>
+            </CardItem>
+            <CardItem>
+              <CardTitle>TitleTitleTitleTitle</CardTitle>
+              <CardText>ああああああああ</CardText>
+            </CardItem>
+            <CardItem>
+              <CardTitle>TitleTitleTitleTitle</CardTitle>
+              <CardText>ああああああああ</CardText>
+            </CardItem>
+            <CardItem>
+              <CardTitle>TitleTitleTitleTitle</CardTitle>
+              <CardText>ああああああああ</CardText>
+            </CardItem>
+            <CardItem>
+              <CardTitle>TitleTitleTitleTitle</CardTitle>
+              <CardText>ああああああああ</CardText>
+            </CardItem>
+            <CardItem>
+              <CardTitle>TitleTitleTitleTitle</CardTitle>
+              <CardText>ああああああああ</CardText>
+            </CardItem>
+          </CardList>
+        </Main>
       </Wrapper>
     )
   }
@@ -48,10 +83,11 @@ const Header = styled.header`
 `
 const Title = styled.h1`
   color: ${white};
-  font-size: 40px;
+  font-size: 42px;
   position: absolute;
   top: 48%;
-  left: 48%;
+  left: 45%;
+  font-weight: bold;
 `
 const BtnMenu = styled.div`
   position: absolute;
@@ -66,9 +102,25 @@ const BtnMenu = styled.div`
     vertical-align: top !important;
   }
 `
-const Sidebar = styled(Drawer)`
-
-`
+const Sidebar = styled(Drawer)``
 const Main = styled.main`
   width: 100%;
+`
+const SubTitle = styled.h2`
+  font-size: 30px;
+  text-align: center;
+  margin: 50px auto 10px;
+  font-weight: bold;
+`
+const CardList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 0 auto;
+  padding: 0 0 0 30px;
+  max-width: 800px;
+`
+const CardItem = styled(Card)`
+  margin: 30px 30px 0 0;
+  flex: 1;
 `
