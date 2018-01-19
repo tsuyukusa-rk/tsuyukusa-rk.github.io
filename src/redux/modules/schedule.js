dad44ea07f53da735f252215615ee70529681af6
tsuyukusa-rk <ryoji@kondouryouji-no-MacBook-Pro.local>
2018-01-19 22:00:23 +0900

    update modules

--- src/redux/modules/schedule.js
+++ src/redux/modules/schedule.js
@@ -20,10 +20,10 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_LIVE_SCHEDULE:
-     return {
-       ...state,
-       ...action.payload
-     }
+      return {
+        ...state,
+        ...action.payload
+      }
     default:
       return state
   }
