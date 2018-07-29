18f194c7d7435c5e733e526812659411ebf041a7
tsuyukusa-rk <>
2018-07-29 19:59:36 +0900

    add audio player

--- site/src/components/audio-player/index.tsx
+++ site/src/components/audio-player/index.tsx
@@ -1,40 +1,100 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import ReactPlayer from 'react-player'
-import { compose, withStateHandlers } from 'recompose'
+import { compose, withStateHandlers, withHandlers } from 'recompose'
 
 interface Props {
   src: string
   title: string,
   isPlaying: boolean,
-  toggleIsPlaying: () => any
+  toggleIsPlaying: () => any,
+  playedSeconds: number,
+  updatePlayedSeconds: (playedSeconds: number) => any,
+  createRef: (ref: any) => any,
+  stopPlaying: () => any,
+  goPlaying: () => any,
+  backPlaying: () => any
 }
 
 export default compose(
   withStateHandlers(
-    { isPlaying: false },
     {
-      toggleIsPlaying: ({ isPlaying }: { isPlaying: boolean }) => () => ({ isPlaying: !isPlaying })
+      isPlaying: false,
+      playedSeconds: 0
+    },
+    {
+      stopIsPlaying: () => () => ({ isPlaying: false }),
+      toggleIsPlaying: ({ isPlaying }: { isPlaying: boolean }) => () => ({ isPlaying: !isPlaying }),
+      updatePlayedSeconds: () => (playedSeconds: number) => ({
+        playedSeconds: Math.round(playedSeconds)
+      })
     }
-  )
+  ),
+  withHandlers(() => {
+    let $ref: any
+    return {
+      createRef: () => (ref: any) => { $ref = ref },
+      stopPlaying: ({ stopIsPlaying }: { stopIsPlaying: () => any }) => () => {
+        stopIsPlaying()
+        $ref.seekTo(0)
+      },
+      goPlaying: () => () => {
+        const currentTime = $ref.getCurrentTime()
+        $ref.seekTo(currentTime + 10)
+      },
+      backPlaying: () => () => {
+        const currentTime = $ref.getCurrentTime()
+        $ref.seekTo(currentTime - 10)
+      }
+    }
+  })
 )(({
   src,
   title,
   isPlaying,
-  toggleIsPlaying
+  toggleIsPlaying,
+  playedSeconds,
+  updatePlayedSeconds,
+  createRef,
+  stopPlaying,
+  goPlaying,
+  backPlaying
 }: Props) => (
   <Wrapper>
     <section className="hero is-light">
-      <ReactPlayer playing={isPlaying} url={src} height='0' width='0' />
+      <ReactPlayer
+        ref={createRef}
+        onProgress={({ playedSeconds }) => { updatePlayedSeconds(playedSeconds) }}
+        playing={isPlaying}
+        url={src}
+        height='0'
+        width='0' />
       <div className="hero-body">
         <div className="container">
-          <p className="title is-4 is-spaced">{title}</p>
+          <Title className="title is-4 is-spaced">
+            <i className="fas fa-signature" />
+            {title}
+          </Title>
           <CtrlWrapper className="subtitle is-flex">
+            <Time>{
+              playedSeconds < 10 ? `0:0${playedSeconds}`
+              : playedSeconds < 60 ? `0:${playedSeconds}`
+              : `${Math.floor(playedSeconds / 60)}:${(playedSeconds % 60) < 10 ? 0 : ''}${playedSeconds % 60}`
+            }</Time>
             <div onClick={!isPlaying && toggleIsPlaying}>
-              <i className="fas fa-play-circle" />
+              <i className="far fa-play-circle" />
             </div>
             <div onClick={isPlaying && toggleIsPlaying}>
-              <i className="fas fa-stop-circle" />
+              <i className="far fa-pause-circle" />
+            </div>
+            <div onClick={stopPlaying}>
+              <i className="far fa-stop-circle" />
+            </div>
+            <div onClick={backPlaying}>
+              <i className="fas fa-angle-double-left" />
+            </div>
+            <div onClick={goPlaying}>
+              <i className="fas fa-angle-double-right" />
             </div>
           </CtrlWrapper>
         </div>
@@ -45,10 +105,22 @@ export default compose(
 
 const CtrlWrapper = styled.div`
   > div {
-    margin-right: 15px;
+    margin-right: 1.5rem;
     cursor: pointer;
   }
 `
 const Wrapper = styled.div`
   margin-top: 4rem;
+`
+const Title = styled.div`
+  > .fa-signature {
+    font-weight: 100;
+    font-size: 1.5rem;
+    color: #999;
+    margin-right: 0.8rem;
+  }
+`
+const Time = styled.div`
+  padding-right: 1.5rem;
+  border-right: 1px solid #aaa;
 `
