77f29bb3161119b9df4bb8c92c027eee828c553c
tsuyukusa-rk <>
2018-07-30 01:05:25 +0900

    update

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -4,6 +4,7 @@ import AudioPlayer from '../components/audio-player'
 import * as amaoto from '../audio/mp3/amaoto.mp3'
 import * as movieStar from '../audio/mp3/movie_star.mp3'
 import * as circleImage from '../images/circle.jpg'
+import * as selfImage from '../images/pic1.jpg'
 
 interface Props {
   data: {
@@ -26,19 +27,25 @@ interface Props {
 
 export default ({ data }: Props) => (
   <Wrapper className='container'>
-    <Section className='section is-medium'>
-      <h3 className='title is-3 has-text-centered'>Biography</h3>
-      <Contents className='content is-small'>
-        <h2>Second level</h2>
-        <p>Curabitur accumsan turpis pharetra <strong>augue tincidunt</strong> blandit. Quisque condimentum maximus mi, sit amet commodo arcu rutrum id. Proin pretium urna vel cursus venenatis. Suspendisse potenti. Etiam mattis sem rhoncus lacus dapibus facilisis. Donec at dignissim dui. Ut et neque nisl.</p>
-        <ul>
-          <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
-          <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
-          <li>Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.</li>
-          <li>Ut non enim metus.</li>
-        </ul>
-      </Contents>
-    </Section>
+    <div className='media'>
+      <Section className='section is-medium'>
+        <h3 className='title is-3 has-text-centered'>Biography</h3>
+        <Contents className='content is-small'>
+          <p>Curabitur accumsan turpis pharetra <strong>augue tincidunt</strong> blandit. Quisque condimentum maximus mi, sit amet commodo arcu rutrum id. Proin pretium urna vel cursus venenatis. Suspendisse potenti. Etiam mattis sem rhoncus lacus dapibus facilisis. Donec at dignissim dui. Ut et neque nisl.</p>
+          <ul>
+            <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
+            <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
+            <li>Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.</li>
+            <li>Ut non enim metus.</li>
+          </ul>
+        </Contents>
+      </Section>
+      <div className="media-right">
+        <p className="image is-256x256">
+          <img src={selfImage} alt='' />
+        </p>
+      </div>
+    </div>
     <Section className='section is-medium'>
       <h3 className='title is-3 has-text-centered'>Live schedule</h3>
       <ul className='columns is-multiline is-3 is-variable'>
